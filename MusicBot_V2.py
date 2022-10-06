6d989fe864019209310432bd9b8ff8d50ae48eb6
Adib-Nasreddine <98001955+Adib-Nasreddine@users.noreply.github.com>
2022-10-05 23:18:40 -0600

    Update MusicBot_V2.py

--- MusicBot_V2.py
+++ MusicBot_V2.py
@@ -1,23 +1,26 @@
-import string
+from pprint import pprint
 import yt_dlp
 import validators
 import discord
 from discord.ext import commands
 from youtube_search import YoutubeSearch 
-from math import fabs
-from traceback import print_tb
 from discord.ext import commands
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-client = commands.Bot(command_prefix="!", intents=intents)
-song_queue = []
-accepted_guilds = ["ABES MOM"]
+client = commands.Bot(command_prefix="$", intents=intents)
 ydl_opts = {'format': 'bestaudio/best'}
 ffmpeg_options = {'options': '-vn',
                   'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'}
+
+# [[song_url], [song_name], [ctx]]
+guilds = {"Radiator Springs": [[], []],
+          "Inmate UNDR17's Cell" : [[], []],
+          "RaticalSurfer's server": [[], []],
+          "Clocktown🕒" : [[], []]
+          }
 
 
 @client.event
@@ -25,70 +28,102 @@ async def on_ready():
     pass
 
 
-def valid_url(songname):
+@commands.is_owner()
+@client.command()
+async def nsfw(ctx,*,song):
+    channel = ctx.message.author.voice.channel
+    voice = await channel.connect()
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        song_info = ydl.extract_info(song, download=False)
+    voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options))
+
+    
+def song_infooooo(songname):  
     valid = validators.url(songname)
+    results = YoutubeSearch(str(songname), max_results=1).to_dict()
     if valid:
-        return songname
+        results[0]["url_suffix"] = songname 
+        return results[0]
     else:
-        results = YoutubeSearch(str(songname), max_results=1).to_dict()
-        prefix = "https://www.youtube.com" + results[0]["url_suffix"]
-        return prefix
+        results[0]["url_suffix"] = "https://www.youtube.com" + results[0]["url_suffix"]
+        return results[0]
     
-    
-def song_name(songname):
-    result = YoutubeSearch(str(songname), max_results=1).to_dict()
-    return result[0]["title"]
+
+def embed_music(ctx, song, song_state, embed_colour):
+    song_information = song_infooooo(song)
+    song_name = song_information["title"]
+    song_duration = song_information["duration"]
+    song_url = song_information["url_suffix"]
+    song_thumbnail = song_information["thumbnails"][0]
+    music_embed = discord.Embed(
+    title = song_state,
+    description = f"[{song_name} [{song_duration}]]({song_url})",
+    colour= embed_colour
+    )
+    music_embed.set_footer(text = f"Added by {ctx.author}", icon_url= ctx.author.avatar)
+    music_embed.set_thumbnail(url= str(song_thumbnail))
+    return music_embed
 
 
-def song_duration(songname):
-    result = YoutubeSearch(str(songname), max_results=1).to_dict()
-    return result[0]["duration"]
-
-    
-    
 swap = False
 @client.command()
 async def play(ctx, *, song):
-    global swap
-    if len(song_queue) != 0 & swap is False:
-        song_queue.pop(0)
-        swap = True
-    channel = ctx.message.author.voice.channel
-    if ctx.author.voice:
-        if ctx.voice_client:
-            voice_status = discord.utils.get(client.voice_clients, guild=ctx.guild)
-            if voice_status.is_playing() or voice_status.is_paused():
-                song_queue.append(valid_url(song))
+    if str(ctx.guild) in guilds:
+        global swap
+        if len(guilds[str(ctx.guild)]) != 0 & swap is False:
+            guilds[str(ctx.guild)][0].pop(0)
+            guilds[str(ctx.guild)][1].pop(0)
+            swap = True
+        song_information = song_infooooo(song)
+        song_url = song_information["url_suffix"]
+        song_name = song_information["title"]
+        channel = ctx.message.author.voice.channel
+        if ctx.author.voice:
+            if ctx.voice_client:
+                voice_status = discord.utils.get(client.voice_clients, guild=ctx.guild)
+                if voice_status.is_playing() or voice_status.is_paused():
+                    guilds[str(ctx.guild)][0].append(song_url)
+                    guilds[str(ctx.guild)][1].append(song_name)
+                    await ctx.send(embed = embed_music(ctx, guilds[str(ctx.guild)][0][-1], "Added to Queue", discord.Colour.red()))
+                else:
+                    guilds[str(ctx.guild)][0].append(song_url)
+                    guilds[str(ctx.guild)][1].append(song_name)
+                    voice = ctx.voice_client
+                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                        song_info = ydl.extract_info(guilds[str(ctx.guild)][0][0], download=False)
+                    await ctx.send(embed = embed_music(ctx, guilds[str(ctx.guild)][0][0], "Now Playing", discord.Colour.blue()))
+                    voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options),after=lambda x=None: song_seq(ctx))
             else:
-                song_queue.append(valid_url(song))
-                voice = ctx.voice_client
+                guilds[str(ctx.guild)][0].append(song_url)
+                guilds[str(ctx.guild)][1].append(song_name)
+                voice = await channel.connect()
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    song_info = ydl.extract_info(valid_url(song_queue[0]), download=False)
+                    song_info = ydl.extract_info(guilds[str(ctx.guild)][0][0], download=False)
+                await ctx.send(embed = embed_music(ctx, guilds[str(ctx.guild)][0][0], "Now Playing", discord.Colour.blue()))
                 voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options),after=lambda x=None: song_seq(ctx))
-                await ctx.send(f"```Now playing {song_name(song_queue[0])} [{song_duration(song_name)}]```")
         else:
-            song_queue.append(valid_url(song))
-            voice = await channel.connect()
-            print(voice)
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                song_info = ydl.extract_info(valid_url(song_queue[0]), download=False)
-            voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options),after=lambda x=None: song_seq(ctx))
-            await ctx.send(f"```Now playing {song_name(song_queue[0])} [{song_duration(song_name)}]```")
+            await ctx.send("```Must be in a voice channel to use this command```")
     else:
-        await ctx.send("```Must be in a voice channel to use this command```")
-
-
+        await ctx.send("```Not an Accepeted guild!```")
+                
+            
 def song_seq(ctx):
     global swap
-    if len(song_queue) != 0:
-        song_queue.pop(0)
-    voice = ctx.voice_client
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        song_info = ydl.extract_info(valid_url(song_queue[0]), download=False)
-    if swap is True:
-        swap = False
-    voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options),after=lambda x=None: song_seq(ctx))
-    ctx.send(f"```Now playing {song_name(song_queue[0])} [{song_duration(song_name)}]```")
+    swap = True
+    if len(guilds[str(ctx.guild)]) != 0:
+        guilds[str(ctx.guild)][0].pop(0)
+        guilds[str(ctx.guild)][1].pop(0)
+        if (len(guilds[str(ctx.guild)][0])) != 0:
+            voice = ctx.voice_client
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                song_info = ydl.extract_info(guilds[str(ctx.guild)][0][0], download=False)
+            channel = client.get_channel(ctx.channel.id)
+            client.loop.create_task(channel.send(embed = embed_music(ctx, guilds[str(ctx.guild)][0][0], "Now Playing", discord.Colour.blue()))) #todo FIX USER QUEUE embed queue adds orginal user
+            voice.play(discord.FFmpegPCMAudio(song_info["url"], **ffmpeg_options),after=lambda x=None: song_seq(ctx))
+        else:
+            pass
+    else:
+        pass
 
 
 @client.command()
@@ -101,7 +136,7 @@ async def pause(ctx):
     else:
         await ctx.send("```No audio is currently playing.```")
 
-
+    
 @client.command()
 async def resume(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
@@ -115,7 +150,7 @@ async def resume(ctx):
 
 @client.command()
 async def stop(ctx):
-    song_queue = 0
+    guilds[str(ctx.guild)] = [[], []]
     if ctx.voice_client:
         await ctx.guild.voice_client.disconnect()
     else:
@@ -124,21 +159,35 @@ async def stop(ctx):
 
 @client.command()
 async def skip(ctx):
-    if len(song_queue) <= 1:
+    if len(guilds[str(ctx.guild)][0]) <= 1:
         await ctx.send("```No song is next in queue for skip command to be used```")
     else:
         voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
         voice.stop()
-        await play(song_queue[0])
+        play(guilds[str(ctx.guild)][0][0])
 
 
-@client.command()
-async def queue(ctx): 
-    song = " "*14 + "Song Queue" + "\n" + "-"*40
-    for songs in song_queue:
-        song += "\n"
-        song += song_name(songs)
-    await ctx.send(f"```{song}```")
+@client.command()                                                                                                                      
+async def queue(ctx):
+    song = ""
+    for i in range(len(guilds[(str(ctx.guild))][0])):
+        song_url = guilds[str(ctx.guild)][0][i]
+        song_name = guilds[str(ctx.guild)][1][i]
+        if i == 0:
+            song += f"Now Playing :headphones:: \n"
+            song += f"[{song_name}]({song_url}) \n\n"
+        elif i == 1:
+            song += f"Up Next :track_next::\n"
+            song += f"{i}: [{song_name}]({song_url}) \n"
+        else:
+            song += f"{i}: [{song_name}]({song_url}) \n"
+    music_embed = discord.Embed(
+    title = f"Song Queue ({len(guilds[(str(ctx.guild))][0])})",
+    description = song,
+    colour = discord.Color.blurple()
+    )
+    music_embed.set_footer(text = f"Requested by {ctx.author}", icon_url= ctx.author.avatar)
+    await ctx.send(embed = music_embed)
         
 
 client.run('ODk1NTUwNjM2NTEzNDU2MTQ4.YV6Mhg.kLla92IQKftCFBCKnShmf2Cmey8')
